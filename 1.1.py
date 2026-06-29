5286dfa632c7696d3a2e913223f79d0a9fb60c41
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-06-29 19:33:27 +0530

    Delete 1.1.py

--- 1.1.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-result = subprocess.run(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-s'], capture_output=True)
-result = result.stdout.decode()
-print(result)
