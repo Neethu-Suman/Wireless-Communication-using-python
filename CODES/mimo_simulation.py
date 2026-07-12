354ed516aa472e47121da67b18f3c6da1513a5d0
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-07-12 08:52:36 +0530

    Update mimo_simulation.py

--- CODES/mimo_simulation.py
+++ CODES/mimo_simulation.py
@@ -4,7 +4,14 @@ n_t = 4
 # Number of receive antennas
 n_r = 2
 # Generating a random MIMO channel matrix
-H = np.random.randn(n_r, n_t)
-
-# Printing the MIMO channel matrix
-print("MIMO Channel Matrix: ", H)
+H = np.random.randn(n_r, n_t) + 1j*np.random.randn(n_r, n_t)
+# Generating a random symbol vector
+x = np.random.randn(n_t) + 1j*np.random.randn(n_t)
+# Generating a noise vector
+n = np.random.randn(n_r) + 1j*np.random.randn(n_r)
+# Generating a received signal vector
+y = H @ x + n
+print("H =", H)
+print("x =", x)
+print("n =", n)
+print("y =", y)
